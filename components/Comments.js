86253fc82cb252d5c4d0afb60a7270c2d341580b
Superoutman <asticosmo@gmail.com>
2022-06-30 10:20:48 +0800

    Update Comments.js

--- components/Comments.js
+++ components/Comments.js
@@ -1,12 +1,27 @@
-import { fetchCusdisLang } from '@/lib/cusdisLang'
 import BLOG from '@/blog.config'
 import dynamic from 'next/dynamic'
+import Tabs from '@/components/Tabs'
+import { useGlobal } from '@/lib/global'
+import React from 'react'
 import { useRouter } from 'next/router'
-import 'gitalk/dist/gitalk.css'
+
+const WalineComponent = dynamic(
+  () => {
+    return import('@/components/WalineComponent')
+  },
+  { ssr: false }
+)
+
+const CusdisComponent = dynamic(
+  () => {
+    return import('@/components/CusdisComponent')
+  },
+  { ssr: false }
+)
 
 const GitalkComponent = dynamic(
   () => {
-    return import('gitalk/dist/gitalk-component')
+    return import('@/components/Gitalk')
   },
   { ssr: false }
 )
@@ -16,50 +31,68 @@ const UtterancesComponent = dynamic(
   },
   { ssr: false }
 )
-const CusdisComponent = dynamic(
+const GiscusComponent = dynamic(
   () => {
-    return import('react-cusdis').then(m => m.ReactCusdis)
+    return import('@/components/Giscus')
   },
   { ssr: false }
 )
 
-const Comments = ({ frontMatter }) => {
-  const router = useRouter()
-  return (
-    <div>
-      {BLOG.comment && BLOG.comment.provider === 'gitalk' && (
-        <GitalkComponent
-          options={{
-            id: frontMatter.id,
-            title: frontMatter.title,
-            clientID: BLOG.comment.gitalkConfig.clientID,
-            clientSecret: BLOG.comment.gitalkConfig.clientSecret,
-            repo: BLOG.comment.gitalkConfig.repo,
-            owner: BLOG.comment.gitalkConfig.owner,
-            admin: BLOG.comment.gitalkConfig.admin,
-            distractionFreeMode: BLOG.comment.gitalkConfig.distractionFreeMode
-          }}
-        />
-      )}
-      {BLOG.comment && BLOG.comment.provider === 'utterances' && (
-        <UtterancesComponent issueTerm={frontMatter.id} />
-      )}
-      {BLOG.comment && BLOG.comment.provider === 'cusdis' && (
-        <CusdisComponent
-        lang={fetchCusdisLang()}
-          attrs={{
-            host: BLOG.comment.cusdisConfig.host,
-            appId: BLOG.comment.cusdisConfig.appId,
-            pageId: frontMatter.id,
-            pageTitle: frontMatter.title,
-            pageUrl: BLOG.link + router.asPath,
-            theme: BLOG.appearance
-          }}
-        />
-      )}
+const ValineComponent = dynamic(() => import('@/components/ValineComponent'), {
+  ssr: false
+})
 
+const Comment = ({ frontMatter }) => {
+  if (!frontMatter) {
+    return <>Loading...</>
+  }
+  const { isDarkMode } = useGlobal()
+  const router = useRouter()
+
+  React.useEffect(() => {
+    // 跳转到评论区
+    setTimeout(() => {
+      if (window.location.href.indexOf('target=comment') > -1) {
+        const url = router.asPath.replace('?target=comment', '')
+        history.replaceState({}, '', url)
+        const commentNode = document.getElementById('comment')
+        commentNode.scrollIntoView({ block: 'start', behavior: 'smooth' })
+      }
+    }, 200)
+  }, [])
+
+  return (
+    <div id='comment' className='comment mt-5 text-gray-800 dark:text-gray-300'>
+      <Tabs>
+
+        { BLOG.COMMENT_WALINE_SERVER_URL && (<div key='Waline'>
+            <WalineComponent/>
+        </div>) }
+
+        {BLOG.COMMENT_VALINE_APP_ID && (<div key='Valine' name='reply'>
+            <ValineComponent path={frontMatter.id}/>
+        </div>)}
+
+        {BLOG.COMMENT_GISCUS_REPO && (
+          <div key="Giscus">
+            <GiscusComponent isDarkMode={isDarkMode} className="px-2" />
+          </div>
+        )}
+
+        {BLOG.COMMENT_CUSDIS_APP_ID && (<div key='Cusdis'>
+          <CusdisComponent frontMatter={frontMatter}/>
+        </div>)}
+
+        {BLOG.COMMENT_UTTERRANCES_REPO && (<div key='Utterance'>
+          <UtterancesComponent issueTerm={frontMatter.id} className='px-2' />
+        </div>)}
+
+        {BLOG.COMMENT_GITALK_CLIENT_ID && (<div key='GitTalk'>
+          <GitalkComponent frontMatter={frontMatter}/>
+        </div>)}
+      </Tabs>
     </div>
   )
 }
 
-export default Comments
+export default Comment
